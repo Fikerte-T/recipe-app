1b37b193e83564bb75a8ad8de3fad4b2640ed190
Fikerte-T <tofikertesfaye@gmail.com>
2022-04-26 18:10:32 +0300

    Add food model

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :foods
-  resources :recipes
-  resources :public_recipes
-  resources :general_shopping_list
-
+  resources :users do
+    resources :general_shopping_list
+    resources :foods
+    resources :recipes do
+      resources :public_recipes
+    end
+  end
   root 'foods#index'
 end
