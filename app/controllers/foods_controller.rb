0db155a8f023202afbbdb0e2a9064729f9446938
Fikerte-T <tofikertesfaye@gmail.com>
2022-04-26 21:20:54 +0300

    Create food list page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,28 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+     @foods = Food.all
+    #  if @foods.empty?
+    #     @foods = 'No food to display'
+    #  else
+    #     @foods
+    #  end
+  end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:food].inspect
+    @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price))
+    respond_to do |format|
+        format.html do
+          if @food.save
+            redirect_to user_foods_path, notice: 'Food created successfully'
+          else
+            flash.now[:error] = 'Error: food could not be saved'
+            render :new, locals: { food: @food }
+          end
+        end
+      end
+  end
 end
