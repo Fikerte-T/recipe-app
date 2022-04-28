6850d2edc1f5356b1a633743f619a6d1115ae17a
Fikerte-T <tofikertesfaye@gmail.com>
2022-04-28 20:19:13 +0300

    Update feature and add style

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_172740) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.float "quantity"
-    t.bigint "user_id", null: false
+    t.bigint "food_id", null: false
     t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
-    t.index ["user_id"], name: "index_recipe_foods_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_172740) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
-  add_foreign_key "recipe_foods", "users"
   add_foreign_key "recipes", "users"
 end
