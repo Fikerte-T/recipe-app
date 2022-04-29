c07469999d96c7b5805c36a2e4b1eb20ba4f9a43
Afiz Savage <savageafiz9@gmail.com>
2022-04-29 09:11:46 +0000

    Created view for recipes page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,19 @@ class RecipesController < ApplicationController
     @user = current_user
     @recipes = Recipe.where(user_id: current_user.id)
   end
+
+  def show
+    @recipe = Recipe.includes(:recipe_foods).find(params[:id])
+  end
+
+  def create
+    @recipe = current_user.recipes.new(params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description))
+    if @recipe.save
+      flash.notice = 'Recipe was successfully created.'
+      redirect_to user_recipes_path(@recipe.user)
+    else
+      flash.alert = 'Recipe was not created.'
+      render :new
+    end
+  end
 end
